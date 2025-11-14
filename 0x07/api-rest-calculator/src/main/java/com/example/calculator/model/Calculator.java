ddef411bf23aae7233e2e5d6e7d0e009a4f858d4
Alan Piovezan <alan.log.piovezan@gmail.com>
2025-11-14 17:47:22 -0300

    bradesco-hbtn-microsservicos-java - 0x07/api-rest-calculator

---
+++ 0x07/api-rest-calculator/src/main/java/com/example/calculator/model/Calculator.java
@@ -0,0 +1,63 @@
+package com.example.calculator.model;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+
+	public Double sum(Double number1, Double number2) {
+		if (number1 == null && number2 == null) {
+			throw new NullPointerException("Número 1 e número 2 são obrigatórios.");
+		}
+
+		return number1 + number2;
+	}
+
+	public Double sub(Double number1, Double number2) {
+		if (number1 == null && number2 == null) {
+			throw new NullPointerException("Número 1 e número 2 são obrigatórios.");
+		}
+
+		return number1 - number2;
+	}
+
+	public Double divide(Double number1, Double number2) {
+		if (number1 == null && number2 == null) {
+			throw new NullPointerException("Número 1 e número 2 são obrigatórios.");
+		} else if (number1 == 0 || number2 == 0) {
+			throw new ArithmeticException("Divisão por zero não é permitido.");
+		}
+
+		return number1 / number2;
+	}
+
+	public Integer factorial(Integer factorial) {
+		if (factorial == null) {
+			throw new NullPointerException("Número é obrigatório.");
+		}
+
+		int result = 1;
+		for (int i = 1; i <= factorial; i++) {
+			result *= i;
+		}
+
+		return result;
+	}
+
+	public Integer integerToBinary(Integer integer) {
+		return Integer.parseInt(Integer.toBinaryString(integer));
+	}
+
+	public String integerToHexadecimal(Integer integer) {
+		return Integer.toHexString(integer);
+	}
+
+	public int calculeDayBetweenDate(LocalDate date1, LocalDate date2) {
+		Period period = Period.between(date1, date2);
+		return period.getDays();
+	}
+
+}
---
+++ 0x07/api-rest-calculator/src/test/java/com/example/calculator/model/CalculatorTest.java
@@ -0,0 +1,77 @@
+package com.example.calculator.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CalculatorTest {
+
+	@InjectMocks
+	private Calculator calculator;
+
+	@Test
+	void sumTest() {
+		Double result = calculator.sum(1.0, 3.0);
+		assertEquals(result, 4.0);
+	}
+
+	@Test
+	public void numbersNullSumTest() {
+		assertThrows(NullPointerException.class, () -> {
+			calculator.sum(null, 1.0);
+		});
+
+		assertThrows(NullPointerException.class, () -> {
+			calculator.sum(3.0, null);
+		});
+	}
+
+	@Test
+	void subTest() {
+		Double result = calculator.sub(10.0, 3.0);
+		assertEquals(result, 7.0);
+	}
+
+	@Test
+	void divideTest() {
+		Double result = calculator.divide(100.0, 2.0);
+		assertEquals(result, 50.0);
+	}
+
+	@Test
+	public void divisionByZeroTest() {
+		assertThrows(ArithmeticException.class, () -> {
+			calculator.divide(3.0, 0.0);
+		});
+	}
+
+	@Test
+	void factorialTest() {
+		assertEquals(calculator.factorial(31), 738197504);
+		assertEquals(calculator.factorial(0), 1);
+	}
+
+	@Test
+	void integerToBinaryTest() {
+		assertEquals(calculator.integerToBinary(12), 1100);
+	}
+
+	@Test
+	void integerToHexadecimalTest() {
+		assertEquals(calculator.integerToHexadecimal(2026), "7ea");
+	}
+
+	@Test
+	void calculeDayBetweenDateTest() {
+		LocalDate date1 = LocalDate.of(2025, 11, 01);
+		LocalDate date2 = LocalDate.of(2025, 11, 30);
+
+		assertEquals(calculator.calculeDayBetweenDate(date1, date2), 29);
+	}
+}
